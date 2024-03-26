49206f32bc3932617ef9714e776f8842432ebd6b
T0p2 <nachoayerbe2003@gmail.com>
2024-03-26 18:03:07 -0300

    Agrego Funcionalidad de carga de archivos

---
+++ upload.php
@@ -0,0 +1,35 @@
+<?php
+// Directorio donde se guardarán los archivos
+$directorioBase = 'materiales/';
+
+// Verificar si se ha enviado un archivo
+if(isset($_FILES['archivo'])) {
+    // Obtener información del archivo
+    $archivoNombre = $_FILES['archivo']['name'];
+    $archivoTipo = $_FILES['archivo']['type'];
+    $archivoTamano = $_FILES['archivo']['size'];
+    $archivoError = $_FILES['archivo']['error'];
+    $archivoTmpNombre = $_FILES['archivo']['tmp_name'];
+
+    // Obtener la materia seleccionada en el formulario
+    $materia = $_POST['materia'];
+
+    // Directorio donde se guardará el archivo según la materia seleccionada
+    $directorioMateria = $directorioBase . '/';
+
+    // Verificar si el directorio de la materia existe, si no, crearlo
+    if (!file_exists($directorioMateria)) {
+        mkdir($directorioMateria, 0777, true);
+    }
+
+    // Mover el archivo al directorio correspondiente
+    $rutaArchivo = $directorioMateria . $archivoNombre;
+    move_uploaded_file($archivoTmpNombre, $rutaArchivo);
+
+    // Mensaje de éxito
+    echo "El archivo se ha cargado correctamente.";
+} else {
+    // Mensaje de error si no se envió ningún archivo
+    echo "Por favor seleccione un archivo para cargar.";
+}
+?>
